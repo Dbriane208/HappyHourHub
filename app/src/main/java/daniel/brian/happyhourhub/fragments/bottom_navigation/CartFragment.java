e9d766ce8e43672c5fe42523cb45b70a56dfa81b
Getange <emmanuelgetange48@gmail.com>
2023-11-21 13:59:58 +0300

    ADD: add to cart functionality

--- app/src/main/java/daniel/brian/happyhourhub/fragments/bottom_navigation/CartFragment.java
+++ app/src/main/java/daniel/brian/happyhourhub/fragments/bottom_navigation/CartFragment.java
@@ -1,22 +1,81 @@
 package daniel.brian.happyhourhub.fragments.bottom_navigation;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import daniel.brian.happyhourhub.R;
 
 public class CartFragment extends Fragment {
 
+    private View view;
+    private LinearLayout cartLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_cart, container, false);
+        view = inflater.inflate(R.layout.fragment_cart, container, false);
+        cartLayout = view.findViewById(R.id.fragment_container);
 
+        // Check if arguments contain image, name, and price
+        if (getArguments() != null) {
+            Bitmap imageBitmap = getArguments().getParcelable("image");
+            String productName = getArguments().getString("productName");
+            String price = getArguments().getString("price");
+
+            // Add a new card to the cart with the received information
+            addCardToCart(imageBitmap, productName, price);
+        }
 
         return view;
+    }
+
+    // Helper method to add a new card to the cart
+    private void addCardToCart(Bitmap imageBitmap, String productName, String price) {
+        // Create a new card layout
+        LinearLayout cardView = new LinearLayout(getContext());
+        cardView.setOrientation(LinearLayout.HORIZONTAL);
+        cardView.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        ));
+
+        // Add ImageView for the image
+        ImageView imageView = new ImageView(getContext());
+        imageView.setImageBitmap(imageBitmap);
+        imageView.setLayoutParams(new LinearLayout.LayoutParams(
+                200, // Adjust the width as needed
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        ));
+
+        // Add TextView for the product name
+        TextView nameTextView = new TextView(getContext());
+        nameTextView.setText(productName);
+        nameTextView.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        ));
+
+        // Add TextView for the price
+        TextView priceTextView = new TextView(getContext());
+        priceTextView.setText(price);
+        priceTextView.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        ));
+
+        // Add the ImageView, product name TextView, and price TextView to the card layout
+        cardView.addView(imageView);
+        cardView.addView(nameTextView);
+        cardView.addView(priceTextView);
+
+        // Add the card layout to the main cart layout
+        cartLayout.addView(cardView);
     }
 }
--- app/src/main/java/daniel/brian/happyhourhub/fragments/bottom_navigation/DescriptionFragment.java
+++ app/src/main/java/daniel/brian/happyhourhub/fragments/bottom_navigation/DescriptionFragment.java
@@ -1,23 +1,75 @@
 package daniel.brian.happyhourhub.fragments.bottom_navigation;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import daniel.brian.happyhourhub.R;
 
 public class DescriptionFragment extends Fragment {
 
+    private View view;
+    private ImageView imageView3;
+    private TextView textViewProductName;
+    private TextView textViewPrice;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-      
-        View view = inflater.inflate(R.layout.fragment_cart, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_description, container, false);
+
+        imageView3 = view.findViewById(R.id.imageView3);
+        textViewProductName = view.findViewById(R.id.textView2);
+        textViewPrice = view.findViewById(R.id.textViewPrice);
+
+        Button addToCartButton = view.findViewById(R.id.addToCartButton);
+        addToCartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the image, product name, and price from the DescriptionFragment
+                Drawable imageDrawable = imageView3.getDrawable();
+                String productName = textViewProductName.getText().toString();
+                String price = textViewPrice.getText().toString();
+
+                // Pass the information to the CartFragment
+                CartFragment cartFragment = new CartFragment();
+                Bundle bundle = new Bundle();
+                bundle.putParcelable("image", drawableToBitmap(imageDrawable));
+                bundle.putString("productName", productName);
+                bundle.putString("price", price);
+                cartFragment.setArguments(bundle);
+
+                // Navigate to the CartFragment
+                FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, cartFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
 
         return view;
     }
 
+    // Helper method to convert Drawable to Bitmap
+    private Bitmap drawableToBitmap(Drawable drawable) {
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+                drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
+    }
 }
