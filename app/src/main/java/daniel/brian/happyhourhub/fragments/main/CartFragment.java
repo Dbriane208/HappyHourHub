e5df774603aea93c0474ec649b9cf4d7e0b4e5cd
Daniel Brian <db9755949@gmail.com>
2023-11-22 11:18:00 +0300

    clean up

--- app/src/main/java/daniel/brian/happyhourhub/fragments/main/CartFragment.java
+++ app/src/main/java/daniel/brian/happyhourhub/fragments/main/CartFragment.java
@@ -1,17 +1,6 @@
 package daniel.brian.happyhourhub.fragments.main;
 
-import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import daniel.brian.happyhourhub.R;
 
 public class CartFragment extends Fragment {
 
