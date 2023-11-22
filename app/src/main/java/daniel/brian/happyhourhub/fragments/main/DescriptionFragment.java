2acf534e03a1f2d73f195ca0959b3b30880618f1
Daniel Brian <db9755949@gmail.com>
2023-11-22 11:16:34 +0300

    clean up

--- app/src/main/java/daniel/brian/happyhourhub/fragments/main/DescriptionFragment.java
+++ app/src/main/java/daniel/brian/happyhourhub/fragments/main/DescriptionFragment.java
@@ -1,24 +1,6 @@
 package daniel.brian.happyhourhub.fragments.main;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.drawable.Drawable;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-
-import daniel.brian.happyhourhub.R;
 
 public class DescriptionFragment extends Fragment {
 
