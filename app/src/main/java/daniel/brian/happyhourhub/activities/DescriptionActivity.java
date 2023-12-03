121d58f7538eb21d1c74b9de3a9466d5ebdc35cd
Daniel Brian <db9755949@gmail.com>
2023-12-03 12:02:33 +0300

    ADD: Sent all data to Description Activity

--- app/src/main/java/daniel/brian/happyhourhub/activities/DescriptionActivity.java
+++ app/src/main/java/daniel/brian/happyhourhub/activities/DescriptionActivity.java
@@ -7,6 +7,8 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
+import com.google.android.material.snackbar.Snackbar;
+
 import java.util.ArrayList;
 
 import daniel.brian.happyhourhub.R;
@@ -24,16 +26,21 @@ public class DescriptionActivity extends AppCompatActivity {
         Intent intent = getIntent();
 
         int adapterPosition = intent.getIntExtra("position",-1);
+        byte[] imageData = intent.getByteArrayExtra("productImage");
         ArrayList<String> productName = intent.getStringArrayListExtra("productName");
         ArrayList<String> productPrice = intent.getStringArrayListExtra("productPrice");
         ArrayList<String> productType = intent.getStringArrayListExtra("productType");
         ArrayList<String> productDescription = intent.getStringArrayListExtra("productDescription");
 
-        if(adapterPosition != -1){
+        if(adapterPosition != -1 && imageData != null){
+            Bitmap bitmap = BitmapFactory.decodeByteArray(imageData,0,imageData.length);
+            descriptionBinding.cocktailImage.setImageBitmap(bitmap);
             descriptionBinding.cocktailName.setText(productName.get(adapterPosition));
             descriptionBinding.cocktailPrice.setText(productPrice.get(adapterPosition));
             descriptionBinding.cocktailType.setText(productType.get(adapterPosition));
             descriptionBinding.cocktailDescription.setText(productDescription.get(adapterPosition));
+        }else{
+            Snackbar.make(this.getCurrentFocus(),"No data Received",Snackbar.LENGTH_LONG).show();
         }
     }
 }
